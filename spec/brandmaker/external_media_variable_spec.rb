7662fafd275c487e91827b824fc4c78f7edc6872
Sebastian de Castelberg <sebu@kpricorn.org>
2013-04-09 11:07:49 +0200

    return self in reload

--- spec/brandmaker/external_media_variable_spec.rb
+++ spec/brandmaker/external_media_variable_spec.rb
@@ -17,17 +17,23 @@ module Brandmaker
       end
 
       context 'with valid mediaID' do
-        it 'fetches media variable attributes from the API' do
+        before :each do
           RestClient.should_receive(:get).with(
             'URL', {:params => {:mediaID => 130, :secret => 'SECRET'}}
           ).and_return({
             :downloadUrl => "download url",
             :fileOriginalName => "original name"
           }.to_json)
+        end
 
+        it 'fetches media variable attributes from the API' do
           var.reload
           var.fileOriginalName.should == 'original name'
           var.downloadUrl.should == 'download url'
+        end
+
+        it 'returns self' do
+          var.reload.should be(var)
         end
       end
 
